0358a64f6832d3a6587736fa2bb6796fc62ded74
matthew-heideman <heidemanmr@yahoo.com>
2020-02-27 14:47:44 -0600

    completed question 2

--- index.js
+++ index.js
@@ -74,7 +74,24 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0,
+    this.odometer = 0
+  }
+  fill(gallons){
+    this.tank += gallons;
+  }
+  drive(distance){
+    this.odometer += distance;
+    this.tank -= distance / this.milesPerGallon;
+    if(this.tank < 0){
+      this.odometer -= Math.abs(this.tank) * this.milesPerGallon;
+      this.tank = 0;
+      return `I ran out of feul at ${this.odometer} miles!`;
+    }
+  }
 }
 
 /*
